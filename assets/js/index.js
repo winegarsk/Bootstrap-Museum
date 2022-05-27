b68747d37c36777781fb2d49ba18f49bca27cecd
winegarsk <33106750+winegarsk@users.noreply.github.com>
2022-05-27 15:11:12 -0400

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
+// const faders = document.querySelectorAll('#fadein');
 
+
+
+// const appearOptions = {};
+
+// const appearOnScroll = new IntersectionObserver
+// (function(
+//     entries,
+//     appearOnScroll
+// ) {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting){
+//             return;
+//         } else {
+//             entry.target.classList.add("appear");
+//             appearOnScroll.unobserve(entry.target);
+//         }
+
+//     })
+// }, appearOptions);
+
+// faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+// })
